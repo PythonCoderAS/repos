c16328d5bd03e63c6e75886c37ea4d18799030b9
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-03-23 22:23:36 -0400

    Fix and debug

--- scripts/get-repo-list.sh
+++ scripts/get-repo-list.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Get a list of all repos, applying excludes, and save it to cache/repo-list-cached.txt
 # If the cache file exists, read from it, otherwise, generate it
+mkdir -p cache
 if [ -f cache/repo-list-cached.txt ]; then
     cat cache/repo-list-cached.txt
     exit 0
